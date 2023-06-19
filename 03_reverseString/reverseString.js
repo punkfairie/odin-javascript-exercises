e96baedad15eef2fa71d608302b10871e523164d
marleyrae <marleyrae@protonmail.com>
2023-06-19 15:33:46 -0700

    Make 03 tests pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversed = ''
 
+  Array.from(string).reverse().forEach(char => {
+    reversed += char
+  })
+
+  return reversed
 };
 
 // Do not edit below this line
