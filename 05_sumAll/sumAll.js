13d8e6b65f97b0f2b7a57818a7a731fef5667465
marleyrae <marleyrae@protonmail.com>
2023-06-19 16:12:59 -0700

    Make 05 tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  if (number1 < 0 || number2 < 0) return 'ERROR'
+  if (typeof number1 !== 'number' || typeof number2 !== 'number') return 'ERROR'
 
-};
+  let sum = 0
+
+  const start = Math.min(number1, number2)
+  const end = Math.max(number1, number2)
+
+  for (let i = start; i <= end; i++) {
+    sum += i
+  }
+
+  return sum
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
