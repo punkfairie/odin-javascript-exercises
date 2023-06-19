6b4c26c0e70b5fe798fe04315d564565bc38654b
marleyrae <marleyrae@protonmail.com>
2023-06-19 16:36:59 -0700

    Make 07 tests pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
-};
+const round = function (number) {
+  return Math.round(number * 10) / 10
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = function (F) {
+  return round((F - 32) * (5 / 9))
+}
+
+const convertToFahrenheit = function (C) {
+  return round((C * (9 / 5)) + 32)
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
-};
+  convertToFahrenheit,
+}
