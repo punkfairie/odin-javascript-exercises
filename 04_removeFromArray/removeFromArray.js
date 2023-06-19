f1149c3915a18ef1b44bc2c7ebd1cccc282b82dc
marleyrae <marleyrae@protonmail.com>
2023-06-19 16:03:48 -0700

    Make 04 tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...remove) {
+  return array.filter(item => !remove.includes(item))
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
