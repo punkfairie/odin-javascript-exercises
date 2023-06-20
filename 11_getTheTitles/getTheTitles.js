efd82eeec7e72b27275c92181672e57cdc008d19
marleyrae <marleyrae@protonmail.com>
2023-06-19 17:23:59 -0700

    Make 11 tests pass

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (books) {
+  return books.map(({title}) => title)
+}
 
 // Do not edit below this line
-module.exports = getTheTitles;
+module.exports = getTheTitles
