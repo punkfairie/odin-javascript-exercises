d12565e62104d570df22767747fe1d89357715e0
marleyrae <marleyrae@protonmail.com>
2023-06-19 17:15:07 -0700

    Make 10 tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (index < 0) return 'OOPS'
 
-};
+  // in loop, add up the sequence until the length of the array is index - 1
+  let seq = [1]
+
+  while (seq.length < index) {
+    if (seq.length === 1) {
+      seq.push(1)
+    } else {
+      seq.push(seq.at(-1) + seq.at(-2))
+    }
+  }
+
+  return seq.pop()
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
