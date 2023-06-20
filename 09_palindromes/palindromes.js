15756b29dd0c5299fe446224fbdf51ed138b4867
marleyrae <marleyrae@protonmail.com>
2023-06-19 17:06:09 -0700

    Make 09 tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.replace(/[^A-Za-z0-9]/g, '').toLowerCase()
+  const reversed = Array.from(string).reverse().reduce((res, cur) => res + cur)
 
-};
+  return string === reversed
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
